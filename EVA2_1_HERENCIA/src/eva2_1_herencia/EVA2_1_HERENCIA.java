1a10943fc4f528e276e588d17ee1f560d9e6ed3f
NocheII <L25550865@chihuahua2.tecnm.mx>
2026-04-13 06:49:10 -0600

    Add files via upload

---
+++ EVA2_1_HERENCIA/src/eva2_1_herencia/EVA2_1_HERENCIA.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_1_herencia;
+
+/**
+ *
+ * @author Moren
+ */
+public class EVA2_1_HERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ejemplo ejemplo = new Ejemplo();
+        System.out.println();
+        // TODO code application logic here
+    }
+    
+}
+
+class Ejemplo{
+}
