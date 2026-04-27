4e4b242fdafe4122198ba3bfb7170274da503ac9
NocheII <L25550865@chihuahua2.tecnm.mx>
2026-04-27 15:35:05 -0600

    Add files via upload

---
+++ EVA2_10_INTERFACES_HERENCIA/src/eva2_10_interfaces_herencia/EVA2_10_INTERFACES_HERENCIA.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_10_interfaces_herencia;
+
+/**
+ *
+ * @author Moren
+ */
+public class EVA2_10_INTERFACES_HERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.mostrarMensaje();
+        // TODO code application logic here
+    }
+    
+}
---
+++ EVA2_10_INTERFACES_HERENCIA/src/eva2_10_interfaces_herencia/Mensaje.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package eva2_10_interfaces_herencia;
+
+/**
+ *
+ * @author Moren
+ */
+public interface Mensaje {
+    
+    //public int valor = 100; 
+    //SOLO METODOS PUBLICOS ABSTRACTOS
+    void mostrarMensaje(); // todos son publicos
+}
